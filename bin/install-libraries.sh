f712ef5fc95b36408a4b2c1582f47cbf8c3e7959
fluxfw <fluxfw@fluxlabs.ch>
2023-03-27 08:38:15 +0200

    `flux-authentication-frontend`

--- bin/install-libraries.sh
+++ bin/install-libraries.sh
@@ -23,7 +23,7 @@ installLibrary() {
 
 checkAlreadyInstalled
 
-installLibrary flux-authentication-frontend https://github.com/fluxfw/flux-authentication-frontend/archive/refs/tags/v2023-03-23-2.tar.gz
+installLibrary flux-authentication-frontend https://github.com/fluxfw/flux-authentication-frontend/archive/refs/tags/v2023-03-27-1.tar.gz
 
 installLibrary flux-css-api https://github.com/fluxfw/flux-css-api/archive/refs/tags/v2023-03-22-1.tar.gz
 
